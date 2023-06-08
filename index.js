d4bd8faa3d981c997e2ee851b2000e1346749224
Pratyushshukla4 <82608141+Pratyushshukla4@users.noreply.github.com>
2023-06-08 16:48:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const dropzone = document.getElementById('dropzone');
 const resetBtn = document.getElementById('resetBtn');
 let successMessage;
 
-// Add event listeners to items for drag events
+
 items.forEach(item => {
   item.addEventListener('dragstart', dragStart);
   item.addEventListener('dragend', dragEnd);
 });
 
-// Add event listeners to dropzone for drop events
+
 dropzone.addEventListener('dragover', dragOver);
 dropzone.addEventListener('dragenter', dragEnter);
 dropzone.addEventListener('dragleave', dragLeave);
 dropzone.addEventListener('drop', drop);
 
-// Add event listener to reset button
+
 resetBtn.addEventListener('click', reset);
 
-// Drag functions
+
 function dragStart(event) {
   event.dataTransfer.setData('text/plain', event.target.textContent);
   event.target.classList.add('dragging');
@@ -28,7 +28,7 @@ function dragEnd(event) {
   event.target.classList.remove('dragging');
 }
 
-// Dropzone functions
+
 function dragOver(event) {
   event.preventDefault();
 }
@@ -52,13 +52,13 @@ function drop(event) {
   showSuccessMessage();
 }
 
-// Reset function
+
 function reset() {
   dropzone.innerHTML = '';
   removeSuccessMessage();
 }
 
-// Success message functions
+
 function showSuccessMessage() {
   if (!successMessage) {
     successMessage = document.createElement('p');
@@ -85,7 +85,7 @@ function drop(event) {
     dropzone.classList.remove('dragover');
     showSuccessMessage();
   
-    // Remove the dropped item from the first container
+    
     const draggedItem = document.querySelector('.dragging');
     draggedItem.remove();
   }
@@ -94,7 +94,7 @@ function drop(event) {
     dropzone.innerHTML = '';
     removeSuccessMessage();
   
-    // Restore all items to the first container
+    
     const container1 = document.querySelectorAll('.container')[0];
     items.forEach(item => container1.appendChild(item));
   }
